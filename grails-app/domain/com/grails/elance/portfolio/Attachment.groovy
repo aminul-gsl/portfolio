5cc649a32b66dc6c811e82c2af528dc0817c1049
Aminul Haque <aminul.haque@grameensolutions.com>
2014-03-20 15:52:14 +0600

    another update

--- grails-app/domain/com/grails/elance/portfolio/Attachment.groovy
+++ grails-app/domain/com/grails/elance/portfolio/Attachment.groovy
@@ -1,6 +1,6 @@
 package com.grails.elance.portfolio
 
-class Attachement {
+class Attachment {
     String name                     //Type of the uploaded file (such as pdf, txt …)
     int typeId
     int size
